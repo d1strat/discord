f8b1eec8831da61658a73d73790e613598a02d7f
Marulia Natalie <77366069+maru1-ux@users.noreply.github.com>
2021-01-13 11:29:31 +0700

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,19 +1,17 @@
 module.exports= {
     name: 'verify',
-    description: "verificat",
+    description: "verification",
     execute(message, args) {
 
+        //you can use this for verificate someone who about to join ur server, by asking them to type .verify and it will automaticaly give them MEMBER roles, note: u should makes MEMBBER/Member/member roles first so this command can work
+        
         
         let role = message.guild.roles.cache.find(r => r.name === "Member");
+      
         if(message.member.roles.cache.some(r => r.name === "Member")){
             message.channel.send('Welcome!');
-           
+            message.member.roles.add(role).catch(console.error); 
 
-
-
-        } else {
-            message.channel.send('You not verified yet!');
-            message.member.roles.add(role).catch(console.error);    
         }
 
 
